04130bba0218aea771a350e4fa175a82d95e1f10
ThonChhayuth <chhayuth0@gmail.com>
2023-12-25 16:23:06 +0700

    second commit

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className=" items-center h-full flex justify-center">{children}</div>
+  );
+};
+
+export default ClerkLayout;
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "Tasktify",
+  description: "Collaborate, manage project, and react new productivity peak",
+};
